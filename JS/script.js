243fb178ab437eb3aeed47ab05bae3ce94fe5a86
Mostafam-Taha <mostafamta347@gmail.com>
2025-02-07 19:41:37 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".home").classList.add("loaded");
     }, 300);
 });
+
+
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navList = document.getElementById('nav-list');
+    navList.classList.toggle('active');
+});
+
+document.addEventListener('click', function(event) {
+    var navList = document.getElementById('nav-list');
+    var menuIcon = document.getElementById('menu-icon');
+    if (event.target !== menuIcon && !navList.contains(event.target)) {
+        navList.classList.remove('active');
+    }
+});
+
+document.getElementById('close-icon').addEventListener('click', function() {
+    var navList = document.getElementById('nav-list');
+    navList.classList.remove('active');
+});
